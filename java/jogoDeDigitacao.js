d28616f6798cde6b790f8b0768d00492144de869
LOGAN ROGES AVELINO <10282224408@edu.fb.org.br>
2025-05-29 19:24:36 -0300

    addEventList

--- java/jogoDeDigitacao.js
+++ java/jogoDeDigitacao.js
@@ -1,39 +1,43 @@
-    let textoOriginal = document.getElementById("texto").textContent
-        let campoTexto = document.getElementById("campoTexto")
-        let resultado = document.getElementById("resultado")
-        let contador = document.getElementById("contador")
-        let reniciarbotao = document.getElementById("btnIniciar")
+     const textoOriginal = document.getElementById('texto').textContent;
+    const campoTexto = document.getElementById('campoTexto');
+    const resultado = document.getElementById('resultado');
+    const contador = document.getElementById('contador');
+    const reiniciarButton = document.getElementById('reiniciar');
+    
+    let tempoIniciado = false;
+    let tempo = 0;
+    let intervalId;
 
-        let tempoIniciado = false
-        let tempo = 0
-        let intervalId;
-        function iniciarTempo(){
-            tempoIniciado = true
-            intervalId = setInterval(()=>{
-                tempo++;
-                contador.textContent = tempo
-            },1000)
-            }
+    function iniciarTempo() {
+      tempoIniciado = true;
+      intervalId = setInterval(function() {
+        tempo++;
+        contador.textContent = tempo;
+      }, 1000);
+    }
 
-            function reiniciarJogo(){
-             tempo = 0
-             contador.textContent = tempo;
-             campoTexto.value = "";
-             campoTexto.disabled = false;
-             tempoIniciado = false;
-            }
+    function reiniciarJogo() {
+      clearInterval(intervalId)
+      tempo = 0;
+      contador.textContent = tempo;
+      campoTexto.value = '';
+      resultado.textContent = '';
+      campoTexto.disabled = false;
+      tempoIniciado = false;
+    }
 
-            campoTexto.addEventListener("input",()=>{
-                if(!tempoIniciado){
-                iniciarTempo();
-                    }
-              if(campoTexto.value === textoOriginal){
-               clearInterval(intervalId);
-               resultado.textContent = `Parabéns, você digitou em ${tempo} segundos!`;
-                campoTexto.disabled = true;
-              }
-            })
+    campoTexto.addEventListener('input', function() {
+      if (!tempoIniciado) {
+        iniciarTempo();
+      }
 
-            reniciarbotao.addEventListener("click",reiniciarJogo())
+      if (campoTexto.value === textoOriginal) {
+        clearInterval(intervalId); 
+        resultado.textContent = `Parabéns! Você digitou o texto em ${tempo} segundos.`;
+        campoTexto.disabled = true; 
+      }
+    });
+
+    reiniciarButton.addEventListener('click', reiniciarJogo);
 
             
